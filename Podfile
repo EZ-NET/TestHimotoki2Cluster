e3c52cea554314159ab14a6106b49efa228d509d
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-04-26 00:14:08 +0900

    Update

---
+++ Podfile
@@ -0,0 +1,8 @@
+target :TestHimotoki2Cluster do
+
+platform :osx, '10.11'
+use_frameworks!
+
+pod 'Himotoki', :git => 'https://github.com/ikesyo/Himotoki.git'
+
+end
--- TestHimotoki2/main.swift
+++ TestHimotoki2/main.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2016 EasyStyle G.K. All rights reserved.
 //
 
-import Foundation
+import Himotoki
+
+let json0: [String : AnyJSON] = [ "type" : 0, "title" : "TITLE 0", "total" : 0.5, "link" : "LINK" ]
+let json1: [String : AnyJSON] = [ "type" : 1, "title" : "TITLE 1", "total" : 1.8, "link" : "LINK" ]
+let json2: [String : AnyJSON] = [ "type" : 2, "title" : "TITLE 2", "total" : 2.3, "link" : "LINK" ]
+
+let instance0 = try! decodeValue(json0) as Cluster
+let instance1 = try! decodeValue(json1) as Cluster
+let instance2 = try! decodeValue(json2) as Cluster
+
+debugPrint(instance0.dynamicType, instance0, instance0.title)   // TestHimotoki2.Cluster TestHimotoki2.B "TITLE 0"
+debugPrint(instance1.dynamicType, instance1, instance1.title)   // TestHimotoki2.Cluster TestHimotoki2.C "TITLE 1"
+debugPrint(instance2.dynamicType, instance2, instance2.title)   // TestHimotoki2.Cluster TestHimotoki2.A "TITLE 2"
