2e4f47b7860da26e008bebd397fe106c68c8beea
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-04-25 22:52:57 +0900

    Initial Commit

---
+++ TestHimotoki2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestHimotoki2
+//
+//  Created by Tomohiro Kumagai on 4/25/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestHimotoki2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestHimotoki2
+//
+//  Created by Tomohiro Kumagai on 4/25/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
